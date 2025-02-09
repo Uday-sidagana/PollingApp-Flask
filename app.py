55273d396179ef34a04635395fc76e49c16f4e29
Uday-sidagana <uday.sidgana@gmail.com>
2025-02-10 00:12:41 +0530

    Test run

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from flask import Flask, redirect, render_template, request, url_for, make_respo
 import os.path
 
 app = Flask(__name__, template_folder="templates")
+
+@app.route('/')
+def index():
+    return "Test"
+
+if  __name__ =="__main__":
+    app.run(host='0.0.0.0', port='5010', debug=True)
