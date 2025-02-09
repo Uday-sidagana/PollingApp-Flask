490d76e6ffd3405c441aba1989e60a463f80b512
Uday-sidagana <uday.sidgana@gmail.com>
2025-02-09 23:53:12 +0530

    libraries and app init

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from flask import Flask
+from flask import Flask, redirect, render_template, request, url_for, make_response
 
 import os.path
 
+app = Flask(__name__, template_folder="templates")
